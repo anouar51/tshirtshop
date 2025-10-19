c206a9ae26f38ed0d3e164a0e440989c98eebe16
anouar51 <smailianouar44@gmail.com>
2025-10-19 11:59:03 +0200

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/ecommerce/tshirtshop/TshirtshopApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.tshirtshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TshirtshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TshirtshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/tshirtshop/TshirtshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.tshirtshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TshirtshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
